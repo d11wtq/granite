fd71c02486a403e5ded95d5e64d04ef97821154b
d11wtq <chris@w3style.co.uk>
2016-10-22 11:29:40 +0000

    Separate program assembly from AST compilation.

---
+++ vm/assembler.go
@@ -0,0 +1,100 @@
+package vm
+
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+// Convert boolean v to a uint8.
+func btoi(v bool) uint8 {
+	if v {
+		return 1
+	}
+
+	return 0
+}
+
+// Bytecode assember for Bijou code
+type Assembler struct {
+	// Program constant pool
+	Constants []Value
+	// List of executable instructions
+	Instructions []uint32
+	// Locations of constants in the constant pool
+	ConstantMap map[Value]uint32
+}
+
+// Create a new bytecode assembler.
+func NewAssembler() *Assembler {
+	return &Assembler{
+		Constants:    make([]Value, 0),
+		Instructions: make([]uint32, 0),
+		ConstantMap:  make(map[Value]uint32),
+	}
+}
+
+// Produce bytecode from the built ASM.
+func (a *Assembler) GetCode() []byte {
+	b := new(bytes.Buffer)
+	a.encodeConstants(b)
+	a.encodeInstructions(b)
+	return b.Bytes()
+}
+
+// Add a constant for use with LOADK.
+// The index of the constant in the pool is returned.
+func (a *Assembler) AddConstant(v Value) uint32 {
+	if _, ok := a.ConstantMap[v]; ok == false {
+		a.ConstantMap[v] = uint32(len(a.Constants))
+		a.Constants = append(a.Constants, v)
+	}
+
+	return a.ConstantMap[v]
+}
+
+// Append an instruction to the list of instructions.
+func (a *Assembler) AddInstruction(inst uint32) {
+	a.Instructions = append(a.Instructions, inst)
+}
+
+// Append a placeholder instruction and return its location.
+func (a *Assembler) ReserveInstruction(op uint32) int {
+	idx := len(a.Instructions)
+	a.AddInstruction(encodeAx(op, 0))
+	return idx
+}
+
+// Replace an existing instruction with another.
+func (a *Assembler) SetInstruction(idx int, inst uint32) {
+	a.Instructions[idx] = inst
+}
+
+func (a *Assembler) encodeConstants(b *bytes.Buffer) {
+	binary.Write(b, ByteOrder, uint32(len(a.Constants)))
+	for _, v := range a.Constants {
+		binary.Write(b, ByteOrder, v.Type())
+		switch t := v.(type) {
+		case *NilType:
+			// noop
+		case Integer:
+			binary.Write(b, ByteOrder, v)
+		case Boolean:
+			binary.Write(b, ByteOrder, btoi(bool(t)))
+		case String:
+			s := []byte(string(t))
+			binary.Write(b, ByteOrder, uint32(len(s)))
+			b.Write(s)
+		}
+	}
+}
+
+func (a *Assembler) encodeInstructions(b *bytes.Buffer) {
+	binary.Write(b, ByteOrder, uint64(len(a.Instructions)+2))
+
+	for _, inst := range a.Instructions {
+		binary.Write(b, ByteOrder, inst)
+	}
+
+	binary.Write(b, ByteOrder, encodeAx(OP_PRINT, 0))
+	binary.Write(b, ByteOrder, encodeAx(OP_RETURN, 0))
+}
--- vm/compile.go
+++ vm/compile.go
@@ -2,8 +2,6 @@ package vm
 
 import (
 	"../parser/ast"
-	"bytes"
-	"encoding/binary"
 	"fmt"
 )
 
@@ -14,23 +12,10 @@ func Compile(prog ast.ASTNode) ([]byte, error) {
 	return compiler.GetCode()
 }
 
-// Convert boolean v to a uint8.
-func btoi(v bool) uint8 {
-	if v {
-		return 1
-	}
-
-	return 0
-}
-
 // Bytecode compiler for Bijou code
 type Compiler struct {
-	// Program constant pool
-	Constants []Value
-	// Locations of constants in the pool (duplicates removed)
-	ConstantMap map[Value]uint32
-	// List of executable instructions
-	Instructions []uint32
+	// Bytecode assembler
+	Assembler *Assembler
 	// Location of local variables in registers
 	LocalMap map[string]uint32
 	// Register where result should be kept
@@ -44,11 +29,9 @@ type Compiler struct {
 // Create a new bytecode compiler.
 func NewCompiler() *Compiler {
 	cmp := &Compiler{
-		Constants:    make([]Value, 0),
-		ConstantMap:  make(map[Value]uint32),
-		Instructions: make([]uint32, 0),
-		LocalMap:     make(map[string]uint32),
-		RegPool:      NewRegisterPool(256),
+		Assembler: NewAssembler(),
+		LocalMap:  make(map[string]uint32),
+		RegPool:   NewRegisterPool(256),
 	}
 
 	cmp.RegIdx = cmp.RegPool.Reserve()
@@ -62,88 +45,37 @@ func (c *Compiler) GetCode() ([]byte, error) {
 		return nil, c.Error
 	}
 
-	b := new(bytes.Buffer)
-	c.encodeConstants(b)
-	c.encodeInstructions(b)
-
-	return b.Bytes(), nil
+	return c.Assembler.GetCode(), nil
 }
 
-func (c *Compiler) encodeConstants(b *bytes.Buffer) {
-	binary.Write(b, ByteOrder, uint32(len(c.Constants)))
-	for _, v := range c.Constants {
-		binary.Write(b, ByteOrder, v.Type())
-		switch t := v.(type) {
-		case *NilType:
-			// noop
-		case Integer:
-			binary.Write(b, ByteOrder, v)
-		case Boolean:
-			binary.Write(b, ByteOrder, btoi(bool(t)))
-		case String:
-			s := []byte(string(t))
-			binary.Write(b, ByteOrder, uint32(len(s)))
-			b.Write(s)
-		}
-	}
-}
-
-func (c *Compiler) encodeInstructions(b *bytes.Buffer) {
-	binary.Write(b, ByteOrder, uint64(len(c.Instructions)+2))
-
-	for _, inst := range c.Instructions {
-		binary.Write(b, ByteOrder, inst)
-	}
-
-	binary.Write(b, ByteOrder, encodeAx(OP_PRINT, 0))
-	binary.Write(b, ByteOrder, encodeAx(OP_RETURN, 0))
-}
-
-func (c *Compiler) addConstant(v Value) {
-	if _, ok := c.ConstantMap[v]; ok == false {
-		c.ConstantMap[v] = uint32(len(c.Constants))
-		c.Constants = append(c.Constants, v)
-	}
-
-	c.addInstruction(encodeAxBx(OP_LOADK, c.RegIdx, c.ConstantMap[v]))
-}
-
-func (c *Compiler) addInstruction(inst uint32) {
-	c.Instructions = append(c.Instructions, inst)
-}
-
-func (c *Compiler) reserveInstruction(int) int {
-	idx := len(c.Instructions)
-	c.addInstruction(0)
-	return idx
-}
-
-func (c *Compiler) setInstruction(idx int, inst uint32) {
-	c.Instructions[idx] = inst
+func (c *Compiler) loadConstant(v Value) {
+	c.Assembler.AddInstruction(
+		encodeAxBx(OP_LOADK, c.RegIdx, c.Assembler.AddConstant(v)),
+	)
 }
 
 func (c *Compiler) returnError(err error) {
 	c.Error = err
-	c.addInstruction(encodeAx(OP_RETURN, 0)) // safeguard-only
+	c.Assembler.AddInstruction(encodeAx(OP_RETURN, 0)) // safeguard-only
 }
 
 func (c *Compiler) VisitNil(node *ast.NilNode) {
-	c.addConstant(Nil)
+	c.loadConstant(Nil)
 }
 
 func (c *Compiler) VisitInteger(node *ast.IntegerNode) {
-	c.addConstant(Integer(node.Value))
+	c.loadConstant(Integer(node.Value))
 }
 
 func (c *Compiler) VisitBoolean(node *ast.BooleanNode) {
-	c.addConstant(Boolean(node.Value))
+	c.loadConstant(Boolean(node.Value))
 }
 
 func (c *Compiler) VisitFloat(node *ast.FloatNode) {
 }
 
 func (c *Compiler) VisitString(node *ast.StringNode) {
-	c.addConstant(String(node.String))
+	c.loadConstant(String(node.String))
 }
 
 func (c *Compiler) VisitSymbol(node *ast.SymbolNode) {
@@ -167,7 +99,7 @@ func (c *Compiler) VisitExpressionList(node *ast.ExpressionList) {
 		}
 
 		if c.RegIdx != regIdx {
-			c.addInstruction(encodeAxBx(OP_MOVE, regIdx, c.RegIdx))
+			c.Assembler.AddInstruction(encodeAxBx(OP_MOVE, regIdx, c.RegIdx))
 			c.RegIdx = regIdx
 		}
 	}
@@ -179,16 +111,16 @@ func (c *Compiler) visitAssignment(node *ast.BinaryExpressionNode) {
 	if ok == false {
 		c.LocalMap[id.Name] = c.RegPool.Reserve()
 		node.Right.Accept(c)
-		c.addInstruction(encodeAxBx(OP_MOVE, c.LocalMap[id.Name], c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBx(OP_MOVE, c.LocalMap[id.Name], c.RegIdx))
 	} else {
 		regA := c.RegIdx
 		regB := c.RegPool.Reserve()
 		c.RegIdx = regB
 		node.Right.Accept(c)
-		c.addInstruction(encodeAxBxCx(OP_EQ, regA, regX, c.RegIdx))
-		c.addInstruction(encodeAxBx(OP_JMPIF, regA, 1))
-		c.addInstruction(encodeAxBxCx(OP_ERR, E_BADMATCH, regX, c.RegIdx))
-		c.addInstruction(encodeAxBx(OP_MOVE, regA, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_EQ, regA, regX, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBx(OP_JMPIF, regA, 1))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_ERR, E_BADMATCH, regX, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBx(OP_MOVE, regA, c.RegIdx))
 		c.RegIdx = regA
 		c.RegPool.Release(regB)
 	}
@@ -211,23 +143,23 @@ func (c *Compiler) VisitBinaryExpression(node *ast.BinaryExpressionNode) {
 
 	switch node.Op {
 	case ast.OP_ADD:
-		c.addInstruction(encodeAxBxCx(OP_ADD, regA, regB, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_ADD, regA, regB, c.RegIdx))
 	case ast.OP_MIN:
-		c.addInstruction(encodeAxBxCx(OP_SUB, regA, regB, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_SUB, regA, regB, c.RegIdx))
 	case ast.OP_MUL:
-		c.addInstruction(encodeAxBxCx(OP_MUL, regA, regB, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_MUL, regA, regB, c.RegIdx))
 	case ast.OP_DIV:
-		c.addInstruction(encodeAxBxCx(OP_DIV, regA, regB, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_DIV, regA, regB, c.RegIdx))
 	case ast.OP_EQL:
-		c.addInstruction(encodeAxBxCx(OP_EQ, regA, regB, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_EQ, regA, regB, c.RegIdx))
 	case ast.OP_LT:
-		c.addInstruction(encodeAxBxCx(OP_LT, regA, regB, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_LT, regA, regB, c.RegIdx))
 	case ast.OP_GT:
-		c.addInstruction(encodeAxBxCx(OP_LT, regA, c.RegIdx, regB))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_LT, regA, c.RegIdx, regB))
 	case ast.OP_LTE:
-		c.addInstruction(encodeAxBxCx(OP_LTE, regA, regB, c.RegIdx))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_LTE, regA, regB, c.RegIdx))
 	case ast.OP_GTE:
-		c.addInstruction(encodeAxBxCx(OP_LTE, regA, c.RegIdx, regB))
+		c.Assembler.AddInstruction(encodeAxBxCx(OP_LTE, regA, c.RegIdx, regB))
 	default:
 		panic(fmt.Sprintf("Unhandled binary operator: 0x%x", node.Op))
 	}
@@ -264,19 +196,19 @@ func (c *Compiler) VisitIfThenElse(node *ast.IfThenElseNode) {
 
 	node.Expression.Accept(c)
 
-	jmpIf = c.reserveInstruction(OP_JMPIF)
+	jmpIf = c.Assembler.ReserveInstruction(OP_JMPIF)
 	node.Else.Accept(c)
-	endElse = c.reserveInstruction(OP_JMP)
+	endElse = c.Assembler.ReserveInstruction(OP_JMP)
 	node.Then.Accept(c)
 
-	c.setInstruction(
+	c.Assembler.SetInstruction(
 		jmpIf,
 		encodeAxBx(OP_JMPIF, regIdx, uint32(endElse-jmpIf)),
 	)
 
-	c.setInstruction(
+	c.Assembler.SetInstruction(
 		endElse,
-		encodeAx(OP_JMP, uint32(len(c.Instructions)-1-endElse)),
+		encodeAx(OP_JMP, uint32(len(c.Assembler.Instructions)-1-endElse)),
 	)
 }
 

1d40e9b34ec4817fb9170f08c0f44cc09d52c730
d11wtq <chris@w3style.co.uk>
2016-11-05 21:06:22 +0000

    Remove unused field.

--- parser/ast/identifier.go
+++ parser/ast/identifier.go
@@ -2,13 +2,12 @@ package ast
 
 // Variable names
 type IdentifierNode struct {
-	Name     string
-	Register uint8
+	Name string
 }
 
 // Return a new identifier with name
 func NewIdentifier(name string) *IdentifierNode {
-	return &IdentifierNode{name, 0}
+	return &IdentifierNode{name}
 }
 
 func (node *IdentifierNode) Accept(visitor ASTVisitor) {
@@ -16,4 +15,4 @@ func (node *IdentifierNode) Accept(visitor ASTVisitor) {
 }
 
 // The special "blank" value
-var Underscore = &IdentifierNode{"_", 0}
+var Underscore = &IdentifierNode{"_"}
